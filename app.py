e02197280b825beb4e4084b28b8d4b51d74c9cd7
reulaw <reureulaw@gmail.com>
2025-02-08 21:37:27 +0700

    tambah templates

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def login():
             return redirect("https://www.google.com/")
         else:
             return "TOLOL"
-    return render_template("login.html")
+    return render_template("auth.html")
 
 
 if __name__ == "__main__":
