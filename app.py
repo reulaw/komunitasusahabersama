26f355bf07c94d8eb993c4f1c2f71f122bbc2a3f
abe <abelaurens@gmail.com>
2025-02-11 07:00:27 +0700

    tambahan

--- app.py
+++ app.py
@@ -7,12 +7,27 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if username == "admin" and password == "1234":
+        if username == "kontol@kontol.com" and password == "kontolkecil":
             return redirect("https://www.google.com/")
         else:
             return "TOLOL"
-    return render_template("auth.html")
+    return render_template("login.html")
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/approval')
+def approval():
+    return render_template('approval.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
